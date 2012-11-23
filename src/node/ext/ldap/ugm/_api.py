8d73928b3dca93a45cabe35d525bd97f82227cc3
Florian Friesdorf <flo@chaoflow.net>
2012-11-23 15:20:17 +0100

    don't load keys of underlying ldap node on init

--- src/node/ext/ldap/ugm/_api.py
+++ src/node/ext/ldap/ugm/_api.py
@@ -403,8 +403,6 @@ class LDAPPrincipals(OdictStorage):
           and cfg.attrmap['login'] != cfg.attrmap['id']:
             context._seckey_attrs += (cfg.attrmap['login'],)
         
-        context._load_keys()
-        
         self.expiresAttr = getattr(cfg, 'expiresAttr', None)
         self.expiresUnit = getattr(cfg, 'expiresUnit', None)
         self.principal_attrmap = cfg.attrmap
